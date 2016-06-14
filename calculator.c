6a1d7e84e34a1596529a00fcf698c4026a788ebc
Uğurcan Polat <ugurcan.polat12@gmail.com>
2016-06-15 00:46:37 +0300

    Correct inputs

--- calculator.c
+++ calculator.c
@@ -1,9 +1,5 @@
-   /* ***************** */
-  /*   Ugurcan Polat   */
- /* polatu@itu.edu.tr */
-/* ***************** */
-
 #include <stdio.h>
+
 #define PI 3.14159265
 
 float add (float x);
@@ -25,12 +21,14 @@ void clear (float* result, float* tempResult);
 void memory (float result, float* tempResult);
 void memoryClear (float* result, float* tempResult);
 float memoryReturn (float result, float tempResult);
-float prnt (float result);
+/* TODO (#1#): ***Refactor 'prnt' function*** */
+/* TODO (#2#): ***Use a new mechanism for 'scanf' *** */
+float prnt (float result); /* This is a proplematic function  */
 void wrong ();
 
 int main (void) {
 	float x = 0, y = 0, result = 0, tempResult = 0;
-	char opertr;
+	char opertr[2];
 	
 	printf ("\n--------------------------------------------------------------\n");
 	printf ("The list of operations in this simple calculator:\nsine (S), cosine (N), tangent (T), exponent (E)\n");
@@ -39,18 +37,29 @@ int main (void) {
 	printf ("--------------------------------------------------------------\n\n");
 	
 	do { //use "do-while" to ask whether the user wants to continue or not
-		if (result == 0 && tempResult == 0)	{ //if x = 0 ask new number to calculate
+		if (result == 0 && tempResult == 0)	{ //if result = 0 ask new number to calculate
 			printf ("Enter integer1: "); //get the number from user
 			scanf ("%f", &x);
 		}
-		else { //if x != 0 use last result as x
+		else { 
 			x = result;
 		}
 		
 		printf ("Enter operator: "); //get operator from user 
-		scanf (" %c", &opertr);
+		scanf ("%s", &opertr);
 		
-			switch (opertr){ // Use a mechanism that converts 'operator' entered by user to  'operatorConverted'
+		if (opertr[1] != 0) {
+			if (opertr[0] == 'M' && opertr[1] == 'C') { //'MC' memory clear
+				opertr[0] = 'X';
+				opertr[1] = 0;
+			}
+			else if (opertr[0] == 'M' && opertr[1] == 'R') { //'MR' memory return
+				opertr[0] = 'Y';
+				opertr[1] = 0;
+			}
+		}
+		
+			switch (opertr[0]) { 
 	            case '+': result = add(x); break; 
 	            case '-': result = subtract(x); break; 
 	            case '*': result = multiply(x); break; 
@@ -71,7 +80,9 @@ int main (void) {
 	            case 'Q': break;
 	            default : wrong ();
 	    	}
-	} while (opertr != 'Q'); 
+	    	opertr[1] = 0;
+	} while (opertr[0] != 'Q'); 
+	return 0;
 } 
 
 float prnt (float result){ //custom print function that prints decimals if result have
@@ -173,20 +184,22 @@ float pi () { //prints defined PI
 	printf ("PI defined as 3.14159265\n");
 	return PI;
 }
-
+	
 double convertRadian(float degree) //function that converts degree to radian
 {
 	double cRadian = (degree / 180) * (PI);
 	return cRadian;
 }
+
 double factorialTaylor(unsigned int number){ //factorial function for taylor series which uses in sine-cosine function
 	if (number == 0) return 1;
 	return number * factorialTaylor(number-1);	
 }
 
-double powerTaylor (float x, int y) { //power function for taylor series which uses in sine-cosine function
+double powerTaylor (float x, int y) { //power function for taylor series which is using by sine-cosine functions
 	double powX = x;
-	for (; y>1; y--) powX *= x; 
+	if (y==0) powX = 1;
+ 	for (; y>1; y--) powX *= x; 
 	return powX;
 }
 
@@ -195,7 +208,7 @@ float sine (float x) { //sine function
 	double radian = convertRadian (x); //calls convertRadian to convert degree to radian 
 	int y;
 	for(y=1; y<10; y++){ //loop to calculate taylor serie for sine
-		int firstPart = powerTaylor (-1, (y+1)); //(-1)^n+1
+		int firstPart = powerTaylor (-1, (y-1)); //(-1)^(n-1)
 		double secondPart = powerTaylor (radian,((2*y)-1)); // x^(2n-1)
 		double thirdPart = factorialTaylor((2*y)-1); // (2n-1)!
 		result = result + firstPart*(secondPart/thirdPart);
@@ -207,13 +220,13 @@ float cosine (float x) { //cosine function
 	float result = 0;
 	double radian = convertRadian (x); //calls convertRadian to convert degree to radian
 	int y;
-	for(y=2; y<10; y++){ //loop to calculate taylor serie for cosine
-		int firstPart = powerTaylor (-1, (y+1)); //(-1)^(n+1)
-		double secondPart = powerTaylor (radian,((2*y)-2)); // x^(2n-2)
+	for(y=1; y<10; y++){ //loop to calculate taylor serie for cosine
+		int firstPart = powerTaylor(-1, (y-1)); //(-1)^(n-1)
+		double secondPart = powerTaylor(radian,((2*y)-2)); // x^(2n-2)
 		double thirdPart = factorialTaylor((2*y)-2); // (2n-2)!
 		result = result + firstPart*(secondPart/thirdPart);
 	}
-	return prnt(result+1);
+	return prnt(result);
 }
 
 float sineT (float x) { //sine function for tangent function
@@ -281,3 +294,4 @@ void wrong (){ //if user enters wrong character or number, prints operators
 	printf ("pi (P) \nQuit (Q) \nMemory (M) \nClear (C) \nMemory Clear (MC) \nMemory Return (MR) \n");
 	printf ("--------------------------------------------------------------\n\n");
 }
+
